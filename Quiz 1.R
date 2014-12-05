a0434701fe68e5e4adca8771af21082e72e4ffde
Mike Taylor <mikecolintaylor@gmail.com>
2014-12-05 17:33:37 +0000

    updated quiz 1 script

--- Quiz 1.R
+++ Quiz 1.R
@@ -1,14 +1,19 @@
 
 setwd("G:/Coursera/Getting and cleaning data")
 
+
+#Q1
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", destfile = "quiz1.csv")
 
 data <- read.csv("quiz1.csv")
 
-
-#Q1
 sum(data$VAL == 24, na.rm = T)
 #3290
 
+
 #Q2
 #Tidy data has one variable per column.
+
+
+#Q3
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx", destfile = "quiz1_3.csv")
