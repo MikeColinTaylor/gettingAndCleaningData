3fdc642251ffa9ec7079e888fc0588a250d828e0
Mike Taylor <mikecolintaylor@gmail.com>
2014-12-05 17:53:10 +0000

    q3 done

--- Quiz 1.R
+++ Quiz 1.R
@@ -1,6 +1,6 @@
 
 setwd("G:/Coursera/Getting and cleaning data")
-
+getwd()
 
 #Q1
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", destfile = "quiz1.csv")
@@ -16,4 +16,18 @@ sum(data$VAL == 24, na.rm = T)
 
 
 #Q3
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx", destfile = "quiz1_3.csv")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx", destfile = "quiz1q3.xlsx", mode = "wb")
+#install.packages("xlsx")
+library("xlsx")
+?read.xlsx
+q3 <- read.xlsx("quiz1q3.xlsx", sheetIndex = 1, rowIndex = c(18:23), colIndex = c(7:15))
+q3
+sum(q3$Zip * q3$Ext, na.rm = T) 
+#36534720
+
+
+#Q4
+#install.packages("XML")
+library("XML")
+?xmlTreeParse
+q4 <- xmlTreeParse("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml", useInternal = T)
