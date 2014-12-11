0e73a25c31a42654440c673c858d2dac82c165e0
Mike Taylor <mikecolintaylor@gmail.com>
2014-12-11 15:39:44 +0000

    course project

--- Quiz 4.R
+++ Quiz 4.R
@@ -22,9 +22,20 @@ q2Data$V4[grep("^United", q2Data$V4)]
 
 
 #Q4
+gdp <- read.csv("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", skip = 5, header = F, nrows = 190)
+edu <- read.csv("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv")
 
+gdpEdu <- merge(x = gdp, y = edu, by.x = "V1", by.y = "CountryCode")
+gdpEdu$Special.Notes[grep("^Fiscal.*June", gdpEdu$Special.Notes)]
+length(grep("^Fiscal.*June", gdpEdu$Special.Notes))
 
 
 #Q5
-
-
+#install.packages("quantmod")
+library(quantmod)
+amzn <- getSymbols("AMZN", auto.assign = F)
+sampleTimes <- index(amzn)
+sum(format(sampleTimes, "%Y") == 2012)
+#250
+sum(format(sampleTimes, "%a") == "Mon" & format(sampleTimes, "%Y") == 2012)
+#47
